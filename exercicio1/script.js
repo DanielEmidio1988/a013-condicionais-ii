416b059467b6611c9f520963c938cded0fd33ab9
DanielEmidio1988 <daniel.emidio.silva@gmail.com>
2022-09-08 16:03:59 -0300

    Exercicio Pratico A013 do dia 08/09

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,19 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite seu numero"))
 
+if (num % 2 === 0){ 
+    if(num %3 === 0){
+        console.log("É divisivel por 2 e 3")
+    }else{
+        console.log("É divisivel por 2")
+    }
+}else if(num %3 === 0){
+    console.log("É divisivel por 3")
+}else{
+    console.log("Não é divisivel nem por 2 ou 3")
+}
+
+if (num%2 === 0 || num%3 === 0){
+    console.log(`O ${num} é divisivel por 2 ou 3`)
+}else{
+    console.log(`O ${num} não é divisivel nem por 2 ou 3`)
+}
